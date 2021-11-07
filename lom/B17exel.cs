9bffc00ebc23feff127ae71b19856500db0fcc63
昶志 林 <dearme61002@gmail.com>
2021-11-07 19:29:47 +0800

    OK

---
+++ lom/B17exel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lom
+{
+    public  class B17exel
+    {
+        public string M_title { get; set; }
+
+        public string M_summary { get; set; }
+
+        public string start_time { get; set; }
+
+        public string end_time { get; set; }
+
+        public string D1_title { get; set; }
+
+        public string name { get; set; }
+
+        public string phone { get; set; }
+
+        public string email { get; set; }
+
+        public int age { get; set; }
+
+        public string writeTime { get; set; }
+
+        public string Answer { get; set; }
+
+    }
+}
